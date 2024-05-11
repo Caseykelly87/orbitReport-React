400294ff939565f48d43fa90f6eeb03b5e4377b1
Casey Kelly <Caseykelly87@gmail.com>
2024-05-11 17:43:32 -0500

    Fully functioning all tests passing, No css added

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,17 +1,27 @@
 const Table = ({ sat }) => {
   return (
     <table>
-    <thead>
-     <tr>
-       <th>Header TBD</th>
-     </tr>
-     </thead>
-     <tbody>
-     <tr>
-       <td>Row Data TBD</td>
-     </tr>
-     </tbody>
-   </table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Type of Satellite</th>
+          <th>Launch Date</th>
+          <th>Status</th>
+        </tr>
+      </thead>
+      <tbody>
+        {sat.map((id, data) => {
+          return<>
+          <tr key={id}>
+          <td>{sat[data].name}</td>
+          <td>{sat[data].type}</td>
+          <td>{sat[data].launchDate}</td>
+          <td>{(sat[data].operational) ? "Active": "Inactive"}</td>
+          </tr>
+          </>
+        })}
+      </tbody>
+    </table>
   );
 };
 
